8b4ad4ea740fbc92838b4f49299f85304571802e
Weronika Kalinowska <wkalinowska406@gmail.com>
2024-12-13 22:00:12 +0100

    Add mui template

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from "react-router-dom";
+import "./App.css";
+import Homepage from "./marketing-page/Homepage";
+import AppTheme from "./shared-theme/AppTheme";
+import AppAppBar from "./marketing-page/components/AppAppBar";
+import Current from "./pages/Current";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<AppAppBar />}>
+      <Route index element={<Homepage />} />
+      <Route path="2025" element={<Current />} />
+    </Route>
   )
+);
+
+function App({ routes }) {
+  return (
+    <AppTheme>
+      <CssBaseline enableColorScheme />
+      <RouterProvider router={router} />
+    </AppTheme>
+  );
 }
 
-export default App
+export default App;
